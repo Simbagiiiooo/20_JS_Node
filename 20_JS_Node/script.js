f362891f06323075579c2069da0888a4c1ccbafd
Simbagiiiooo <67731418+Simbagiiiooo@users.noreply.github.com>
2020-12-30 13:15:57 +0100

    Add files via upload

---
+++ 20_JS_Node/script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy Studenten");
+// dit is een grote som! (deze comment zie je niet in de console omdat deze niet meedoet aan de js engine. )
+/* let sumMultiplication = 1230941 * 1823794;
+console.log(sumMultiplication);
+let sumDivision = 637263 / 54;
+console.log(sumDivision); */
+// De uitkomst zie je niet, omdat ze met een block comment niet meedoen in de engine.
